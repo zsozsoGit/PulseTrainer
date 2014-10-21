e4304f0ad515f2744621b23b0e0be14c6eb6629b
zs <zsozso.80@gmail.com>
2014-10-21 23:24:11 +0200

    Better

--- app/build/generated/source/r/debug/com/example/stapp/R.java
+++ app/build/generated/source/r/debug/com/example/stapp/R.java
@@ -22,7 +22,7 @@ public final class R {
         public static final int TableLayout1=0x7f080001;
         public static final int TextView03=0x7f080002;
         public static final int action_settings=0x7f080004;
-        public static final int textView2=0x7f080000;
+        public static final int textView=0x7f080000;
     }
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
@@ -33,18 +33,19 @@ public final class R {
         public static final int main=0x7f070001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050000;
-        public static final int app_name=0x7f050001;
-        public static final int button_send=0x7f050002;
-        public static final int hello_world=0x7f050003;
-        public static final int number_of_reps=0x7f050004;
-        public static final int rest_length=0x7f050005;
-        public static final int run_length=0x7f050006;
+        public static final int BigText=0x7f050000;
+        public static final int action_settings=0x7f050001;
+        public static final int app_name=0x7f050002;
+        public static final int button_send=0x7f050003;
+        public static final int hello_world=0x7f050004;
+        public static final int number_of_reps=0x7f050005;
+        public static final int rest_length=0x7f050006;
+        public static final int run_length=0x7f050007;
         /**  Comment? TODO 
          */
-        public static final int speed=0x7f050007;
-        public static final int title_activity_display_message=0x7f050008;
-        public static final int title_activity_main=0x7f050009;
+        public static final int speed=0x7f050008;
+        public static final int title_activity_display_message=0x7f050009;
+        public static final int title_activity_main=0x7f05000a;
     }
     public static final class style {
         /** 
